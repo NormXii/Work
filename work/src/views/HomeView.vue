d312e05b4070069c6987b505931b4bb2dda2de6b
Emma Thompson <1245989664@qq.com>
2024-05-16 12:45:47 +0800

    1

--- work/src/views/HomeView.vue
+++ work/src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <HelloWorld msg="欢迎您，Vue.js App！"/>
   </div>
 </template>
 
