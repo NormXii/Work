5df6d7962e97ff249b3318c137e51500af8b0fd2
Emma Thompson <1245989664@qq.com>
2024-05-17 19:12:35 +0800

    update

--- work/src/views/HomeView.vue
+++ work/src/views/HomeView.vue
@@ -3,12 +3,27 @@
     <el-container>
       <!-- 侧边栏 -->
       <el-aside>
+        <div style="height: 60px; line-height: 60px; background-color: #8c939d; color: red; display: flex; align-items: center; justify-content: center;">
+          Logo
+        </div>
+        
+        <el-menu>
+          <el-menu-item>系统首页</el-menu-item>
+          <el-submenu>
+            <template v-slot:title>
+              <i class="el-icon-menu"></i>
+              <span>信息管理</span>
+            </template>
+            <el-menu-item>用户信息</el-menu-item>
+          </el-submenu>
+        </el-menu>
 
       </el-aside>
       <el-container>
         <!-- 头部区域 -->
               
         <el-header>
+          头部区域
 
        </el-header>
 
@@ -17,6 +32,7 @@
 
         <el-main>
 
+          主体区域
 
         </el-main>
        </el-container>
